0c2f69fb58336f3bfced23f2d12c211222314f54
avinasxhhh <avinashmundhe1050@gmail.com>
2023-10-27 19:45:54 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+  searchForm.classList.toggle('active');
+}
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+
+  searchForm.classList.remove('active');
+
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }else{
+    document.querySelector('.header .header-2').classList.remove('active');
+  }
+
+}
+
+window.onload = () =>{
+
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }else{
+    document.querySelector('.header .header-2').classList.remove('active');
+  }
+
+  fadeOut();
+
+}
+
+
+var scrollAnimElements = document.querySelectorAll("[data-animate-on-scroll]");
+var observer = new IntersectionObserver(
+  (entries) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting || entry.intersectionRatio > 0) {
+        const targetElement = entry.target;
+        targetElement.classList.add("animate");
+        observer.unobserve(targetElement);
+      }
+    }
+  },
+  {
+    threshold: 0.15,
+  }
+);
+
+for (let i = 0; i < scrollAnimElements.length; i++) {
+  observer.observe(scrollAnimElements[i]);
+}
